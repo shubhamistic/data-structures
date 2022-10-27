b1374e877989b9be294b35e87ee27114b7d1d87d
Shubham <shubham.219301072@muj.manipal.edu>
2022-10-27 10:06:11 +0530

    modified code

--- stack_using_array.c
+++ stack_using_array.c
@@ -3,24 +3,34 @@
 
 //function-definitions;
 int mainMenu();
-void push();
-int pop();
-void display();
+void push(int stack[], const int* size, int* top);
+int pop(int stack[], int* size, int* top);
+void display(int stack[], int top);
 
 int main(){
-    int choice;
 
+    int size, top=-1;
+    printf("\nEnter size of the stack: ");
+    scanf("%d", &size);
+    int* stack = (int*)calloc(size, sizeof(int));
+    if (stack == NULL) {
+        printf("\n:( Memory not allocated.\n");
+        exit(0);
+    }
+    printf("\n:) Stack successfully Created.\n");
+
+    int choice;
     do {
         choice = mainMenu();
         switch (choice) {
             case 1:
-                push();
+                push(stack, &size, &top);
                 break;
             case 2:
-                pop();
+                pop(stack, &size, &top);
                 break;
             case 3:
-                display();
+                display(stack, top);
                 break;
             case 4:
                 break;
@@ -43,4 +53,39 @@ int mainMenu(){
     printf("\nEnter Choice: ");
     scanf("%d", &choice);
     return choice;
+}
+
+void push(int stack[], const int* size, int* top){
+    if(*top == *size-1){
+        printf("\n:( Stack Overflow.\n");
+        return;
+    }
+    int n;
+    printf("\nEnter Element to push: ");
+    scanf("%d", &n);
+    stack[++(*top)] = n;
+    printf("\n:) PUSH SUCCESSFUL.\n");
+}
+
+
+int pop(int stack[], int* size, int* top){
+    if(*top == -1){
+        printf("\n:( Stack Underflow.\n");
+        return -1;
+    }
+    printf("\n:) POP SUCCESSFUL.\nDATA VALUE: '%d'\n", stack[*top]);
+    return stack[(*top)--];
+}
+
+void display(int stack[], int top){
+    if(top == -1){
+        printf("\n:( Stack is empty.\n");
+        return;
+    }
+    printf("\n");
+    for(int i=0 ; i<=top ; i++){
+        printf("%d,", stack[top-i]);
+    }
+    printf("\n^");
+    printf("\nTOP\n");
 }
