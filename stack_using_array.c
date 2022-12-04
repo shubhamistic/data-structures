f5aa81c6acd67b54a4b69b8cb974dec7b87f0a0e
Shubham <shubham.219301072@muj.manipal.edu>
2022-12-04 15:57:23 +0530

    modified Stack using array

--- stack_using_array.c
+++ stack_using_array.c
@@ -1,41 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//structure of a stack.
+struct Stack{
+    int* array;
+    int size;
+    int top;
+};
+typedef struct Stack Stack;
+
 //function-definitions;
 int mainMenu();
-void push(int stack[], const int* size, int* top);
-int pop(int stack[], int* size, int* top);
-void display(int stack[], int top);
+void push(Stack* s);
+int pop(Stack* s);
+void display(const Stack* s);
 
 int main(){
-
-    int size, top=-1;
+    Stack* s = (Stack*)malloc(sizeof(Stack));
+    s->top = -1;
     printf("\nEnter size of the stack: ");
-    scanf("%d", &size);
-    int* stack = (int*)calloc(size, sizeof(int));
-    if (stack == NULL) {
+    scanf("%d", &(s->size));
+    s->array = (int*)calloc(s->size, sizeof(int));
+    if (s->array == NULL) {
         printf("\n:( Memory not allocated.\n");
         exit(0);
     }
-    printf("\n:) Stack successfully Created.\n");
+    printf("\n:) Stack of size '%d' is successfully Created.\n", s->size);
 
     int choice;
     do {
         choice = mainMenu();
         switch (choice) {
             case 1:
-                push(stack, &size, &top);
+                push(s);
                 break;
             case 2:
-                pop(stack, &size, &top);
+                pop(s);
                 break;
             case 3:
-                display(stack, top);
+                display(s);
                 break;
             case 4:
+                free(s->array);
+                free(s);
                 break;
             default:
-                printf(":( Enter valid operation number.\n\n");
+                printf("\n:( Enter valid operation number.\n");
         }
     }
     while(choice!=4);
@@ -55,37 +65,38 @@ int mainMenu(){
     return choice;
 }
 
-void push(int stack[], const int* size, int* top){
-    if(*top == *size-1){
+void push(Stack* s){
+    if(s->top == s->size-1){
         printf("\n:( Stack Overflow.\n");
         return;
     }
     int n;
     printf("\nEnter Element to push: ");
     scanf("%d", &n);
-    stack[++(*top)] = n;
+    s->array[++(s->top)] = n;
     printf("\n:) PUSH SUCCESSFUL.\n");
 }
 
-
-int pop(int stack[], int* size, int* top){
-    if(*top == -1){
+int pop(Stack* s){
+    if(s->top == -1){
         printf("\n:( Stack Underflow.\n");
         return -1;
     }
-    printf("\n:) POP SUCCESSFUL.\nDATA VALUE: '%d'\n", stack[*top]);
-    return stack[(*top)--];
+    printf("\n:) POP SUCCESSFUL.\nDATA VALUE: '%d'\n", s->array[s->top]);
+    return s->array[(s->top)--];
 }
 
-void display(int stack[], int top){
-    if(top == -1){
+void display(const Stack* s){
+    if(s->top == -1){
         printf("\n:( Stack is empty.\n");
         return;
     }
     printf("\n");
-    for(int i=0 ; i<=top ; i++){
-        printf("%d,", stack[top-i]);
+    for(int i=0 ; i<=s->top ; i++){
+        printf("%d,", s->array[s->top-i]);
     }
     printf("\n^");
     printf("\nTOP\n");
 }
+
+//By - Shubham 219301072 B.Tech Sec-3C (CSE);
